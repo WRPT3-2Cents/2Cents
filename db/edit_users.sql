1ea6eb50ef0166ac73ae675a2d38d04988f1d92a
Nathen Amestoy <nate1347@gmail.com>
2021-12-18 16:59:21 -0700

    sql updates and register component

--- db/edit_users.sql
+++ db/edit_users.sql
@@ -1,5 +1,5 @@
 UPDATE Users;
-SET username = $2 , password = $3, email = $4, recommendations = $4, watchlist = $5
+SET username = $2 , password = $3, email = $4, recommendations = $5, watchlist = $6
 WHERE user_id = $1;
 
 SELECT * FROM Users;
