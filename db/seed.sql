afd60e4825971b278c83981e97eb8bf3254b2f0f
Jordan <acey1jordan@gmail.com>
2021-12-18 14:26:32 -0700

    Jordan | Finished displaying movies from API in home

---
+++ db/seed.sql
@@ -0,0 +1,24 @@
+Titles (
+    title_id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    type VARCHAR(30),
+    summary TEXT,
+    genre VARCHAR(30),
+    length INT,
+    recommendations INT,
+    non_recommendations INT
+Users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50),
+    password TEXT,
+    email VARCHAR(50),
+    recommendations VARCHAR(100)[],
+    watchlist VARCHAR(100)[]
+Comments (
+    comment_id SERIAL PRIMARY KEY,
+    message TEXT,
+    date DATE,
+    user_id INT REFERENCES Users (user_id),
+    title_id INT REFERENCES Titles (title_id),
+    previous_id INT,
+    next_id INT
