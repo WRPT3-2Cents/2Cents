51c765108f0610176b0ddc47d4c522e17a8960e6
Joey-Perry <Joeyap84@gmail.com>
2021-12-28 12:14:35 -0600

    Added logic to hide comment functionality when user is not logged in

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -5,13 +5,6 @@ import './menu.css';
 const Menu = () => {
 
     return (
-        // links.map(link => {
-        //     if (link === 'Home'){
-        //         return <li><Link to='/'>Home</Link></li>
-        //     }
-            
-        //     return <li><Link to={`/${link}`}>{link}</Link></li>
-        // })
         <div className='menu'>
             <ul className='nav-links'>
                 <li><h4><Link to='/'>Home</Link></h4></li>
