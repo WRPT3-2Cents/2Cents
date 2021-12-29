c9b896de936869d3c4d24a0b45f4a43903e52080
Nathen Amestoy <nate1347@gmail.com>
2021-12-28 18:25:51 -0700

    css for reg and login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,50 +1,55 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { Link} from "react-router-dom";
-import './login.css'
-
+import { Link } from "react-router-dom";
+import "./login.css";
+import { MDBCol, MDBInput} from "mdbreact";
 
 const Login = () => {
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
-  
-  
 
   const loginUser = () => {
-      const body = {
-          userName,
-          password
-      };
+    const body = {
+      userName,
+      password,
+    };
 
-      axios.post("/api/login", body)
-      .then((res)=> {
-         
-      })
-  }
+    axios.post("/api/login", body).then((res) => {});
+  };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={loginUser} className="login-form">
-        <input
-        className="login-inputs"
-          placeholder="USERNAME"
-          type="text"
-          value={userName}
-          onChange={(e) => setUserName(e.target.value)}
-        />
-        <input
-        className="login-inputs"
-          placeholder="PASSWORD"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Link to="/Follows"><button>Login</button></Link>
-        <Link to="/Sign-up">
-          <p>Create An Account</p>
-        </Link>
-      </form>
+    <div className="login-container" >
+      <MDBCol>
+        <form onSubmit={loginUser}>
+          <h1 className="h1 text-center mb-4">Login</h1>
+          <div className="black-text">
+          <MDBInput
+            label="Enter your username"
+            group
+            type="text"
+            validate
+            error="wrong"
+            success="right"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+          <MDBInput
+            label="Enter your password"
+            group
+            type="password"
+            validate
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          </div>
+          <Link to="/Follows">
+            <button>Login</button>
+          </Link>
+          <Link to="/Sign-up">
+            <p className="h6 text-center mb-4">Create An Account</p>
+          </Link>
+        </form>
+      </MDBCol>
     </div>
   );
 };
